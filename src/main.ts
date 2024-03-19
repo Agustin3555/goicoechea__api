18dc75354bd49ec0925f979c0c170e2b6807726f
Agustin3555 <Agustin3555@hotmail.com>
2024-03-19 16:18:27 -0300

    feat(): Agrega el uso de Fastify y SWC. Elimina los archivos no usados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
+
+  app.setGlobalPrefix('api/v2')
+
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap()
